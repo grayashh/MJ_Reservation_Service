f5eebe9787af7c041aa874f6f95c996b7586f8fd
bokyeom <bk@bo.local>
2022-07-07 20:53:51 +0900

    feat: 로그인, 회원가입 구현

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const config =
+  require('../config/mysql')[process.env.NODE_ENV || 'development'];
+
+const User = require('./user');
+
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+
+db.User = User;
+
+User.init(sequelize);
+
+module.exports = db;
