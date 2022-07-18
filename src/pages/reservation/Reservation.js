1f7912f1da33e82bc7c2f4590e6d6a820d0d19f0
kimtaekang <k_2684@naver.com>
2022-07-18 23:51:19 +0900

    feat : 예약페이지 구현

---
+++ src/pages/reservation/Reservation.js
@@ -0,0 +1,113 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import ReservationForm from "../../components/ReservationForm";
+import PrivacyForm from "../../components/PrivacyForm";
+import Result from "../../components/Result";
+
+const steps = ["Shipping address", "Payment details", "Result your order"];
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <ReservationForm />;
+    case 1:
+      return <PrivacyForm />;
+    case 2:
+      return <Result />;
+    default:
+      throw new Error("Unknown step");
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#005cb8",
+    },
+    secondary: {
+      main: "#051c48",
+    },
+    header: {
+      main: "#808080",
+    },
+    typography: {
+      fontFamily: "'Noto Sans KR', sans-serif",
+    },
+  },
+});
+
+export default function Checkout() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="sm">
+        <Paper
+          variant="outlined"
+          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+        >
+          <Typography variant="h4" align="center">
+            예약하기
+          </Typography>
+          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <React.Fragment>
+            {activeStep === steps.length ? (
+              <React.Fragment>
+                <Typography variant="h5" gutterBottom>
+                  Thank you for your order.
+                </Typography>
+                <Typography variant="subtitle1">
+                  Your order number is #2001539. We have emailed your order
+                  confirmation, and will send you an update when your order has
+                  shipped.
+                </Typography>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {getStepContent(activeStep)}
+                <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                  {activeStep !== 0 && (
+                    <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                      Back
+                    </Button>
+                  )}
+
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 3, ml: 1 }}
+                  >
+                    {activeStep === steps.length - 1 ? "Place order" : "Next"}
+                  </Button>
+                </Box>
+              </React.Fragment>
+            )}
+          </React.Fragment>
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
+}
