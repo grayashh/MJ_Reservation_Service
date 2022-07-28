a6e804e9c3994edbc4f265aef5d321c76791b48c
kimtaekang <k_2684@naver.com>
2022-07-28 13:08:56 +0900

    feat : 예약 확인 페이지 구현

---
+++ src/pages/check/CheckResult.js
@@ -0,0 +1,116 @@
+import { Container, createTheme, Grid, Paper, Typography } from "@mui/material";
+import axios from "axios";
+import * as React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import {
+  ActivityTimeline,
+  DateTimePicker,
+  Input,
+  TimelineMarker,
+} from "react-rainbow-components";
+import { ThemeProvider } from "@mui/material";
+import Header from "../../components/Header";
+import FadeIn from "react-fade-in/lib/FadeIn";
+import BackgroundVideo from "../../components/Background";
+
+const CheckResult = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("/users").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  let printArea = users.area === "RIGHT" ? "오른쪽 코트" : "왼쪽 코트";
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#005cb8",
+      },
+      secondary: {
+        main: "#051c48",
+      },
+      header: {
+        main: "transparent",
+      },
+      icon: {
+        main: "#FFFFFF",
+      },
+      typography: {
+        fontFamily: "'Noto Sans KR', sans-serif",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <FadeIn>
+        <Container component="main" maxWidth="md">
+          <Paper variant="outlined" sx={{ p: { xs: 6 } }}>
+            <Grid container spacing={2} alignContent="center">
+              <ActivityTimeline>
+                <Grid container spacing={6}>
+                  <Grid item xs={12}>
+                    <Typography variant="h4" fontWeight="bold">
+                      예약정보
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TimelineMarker label="예약 시작시간" />
+                    <DateTimePicker
+                      readOnly
+                      value={new Date(users.startValue)}
+                      isCentered
+                      icon=" "
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TimelineMarker label="예약 종료시간" />
+                    <DateTimePicker
+                      readOnly
+                      value={new Date(users.endValue)}
+                      isCentered
+                      icon=" "
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TimelineMarker label="예약 장소" />
+                    <Input readOnly value={printArea} isCentered></Input>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TimelineMarker label="예약 인원" />
+                    <Input
+                      readOnly
+                      value={users.headCounter + "명"}
+                      isCentered
+                    ></Input>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TimelineMarker label="예약자명" />
+                    <Input readOnly value={users.name} isCentered></Input>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TimelineMarker label="예약자 전화번호" />
+                    <Input readOnly value={users.phone} isCentered></Input>
+                  </Grid>
+                </Grid>
+              </ActivityTimeline>
+            </Grid>
+          </Paper>
+        </Container>
+      </FadeIn>
+      <BackgroundVideo />
+    </ThemeProvider>
+  );
+};
+
+export default CheckResult;
