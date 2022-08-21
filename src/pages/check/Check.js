1c70d451a07221bc6a6b926e4117ac27f10d7f45
k-kbk <kkbk-@naver.com>
2022-08-21 16:03:06 +0900

    fix: back-end 연결을 위한 수정

--- src/pages/check/Check.js
+++ src/pages/check/Check.js
@@ -1,22 +1,22 @@
-import { Grid, Container } from "@mui/material";
-import axios from "axios";
-import React, { useState } from "react";
-import { Application, Button, Input } from "react-rainbow-components";
-import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
+import { Grid, Container } from '@mui/material';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Application, Button, Input } from 'react-rainbow-components';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 export default function Check() {
   const theme = {
     rainbow: {
       palette: {
-        brand: "#005cb8",
+        brand: '#005cb8',
       },
     },
   };
 
   // 예약 비밀번호 확인 state
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
   // 이름 입력 handle
   const handleOnName = (event) => {
@@ -42,24 +42,24 @@ export default function Check() {
     console.log(postData);
 
     await axios
-      .post("/check", { postData })
+      .post('http://localhost:7000/reservation/find', postData)
       .then((res) => {
         Swal.fire({
-          icon: "success",
-          title: "조회 성공",
+          icon: 'success',
+          title: '조회 성공',
           showConfirmButton: false,
           timer: 1000,
         });
         // 성공시 navigate
-        navigate("/my/reservation");
+        navigate('/my/reservation');
       })
       .catch((err) => {
         Swal.fire({
-          icon: "error",
-          iconColor: "#d32f2f",
-          title: "조회 실패",
-          text: "다시 시도해주세요",
-          confirmButtonColor: "#005cb8",
+          icon: 'error',
+          iconColor: '#d32f2f',
+          title: '조회 실패',
+          text: '다시 시도해주세요',
+          confirmButtonColor: '#005cb8',
         });
       });
   };
@@ -77,41 +77,40 @@ export default function Check() {
 
   return (
     <Application theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component='main' maxWidth='xs'>
         <Grid
           container
           sx={{
-            height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            alignContent: "center",
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignContent: 'center',
           }}
-          spacing={1}
-        >
+          spacing={1}>
           <Grid item xs={12}>
             <Input
-              id="name"
-              label="이름을 입력하세요"
-              type="name"
+              id='name'
+              label='이름을 입력하세요'
+              type='name'
               onChange={handleOnName}
             />
           </Grid>
 
           <Grid item xs={12}>
             <Input
-              id="password"
-              label="예약 확인용 비밀번호를 입력하세요"
-              type="password"
+              id='password'
+              label='예약 확인용 비밀번호를 입력하세요'
+              type='password'
               onChange={handleOnPassword}
             />
           </Grid>
           <Grid item xs={12}>
             <Button
               shaded
-              type="submit"
-              variant="brand"
-              label="제출"
+              type='submit'
+              variant='brand'
+              label='제출'
               onClick={(event) => handleSubmit(event)}
             />
           </Grid>
--- src/pages/reservation/Reservation.js
+++ src/pages/reservation/Reservation.js
@@ -1,39 +1,39 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Paper from "@mui/material/Paper";
-import Stepper from "@mui/material/Stepper";
-import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import ReservationForm from "../../components/ReservationForm";
-import PrivacyForm from "../../components/PrivacyForm";
-import Result from "../../components/Result";
-import Header from "../../components/Header";
-import BackgroundVideo from "../../components/Background";
-import { useState } from "react";
-import Swal from "sweetalert2";
-import axios from "axios";
-import FadeIn from "react-fade-in/lib/FadeIn";
-import { useNavigate } from "react-router-dom";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ReservationForm from '../../components/ReservationForm';
+import PrivacyForm from '../../components/PrivacyForm';
+import Result from '../../components/Result';
+import Header from '../../components/Header';
+import BackgroundVideo from '../../components/Background';
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import FadeIn from 'react-fade-in/lib/FadeIn';
+import { useNavigate } from 'react-router-dom';
 
-const steps = ["개인정보 동의", "예약정보 작성", "예약 완료"];
+const steps = ['개인정보 동의', '예약정보 작성', '예약 완료'];
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#005cb8",
+      main: '#005cb8',
     },
     secondary: {
-      main: "#051c48",
+      main: '#051c48',
     },
     header: {
-      main: "transparent",
+      main: 'transparent',
     },
     icon: {
-      main: "#FFFFFF",
+      main: '#FFFFFF',
     },
     typography: {
       fontFamily: "'Noto Sans KR', sans-serif",
@@ -46,35 +46,36 @@ export default function Checkout() {
     const { startValue, endValue, area, headCount, name, phone, password } =
       data;
     const postData = {
-      startValue,
-      endValue,
-      area,
-      headCount,
       name,
       phone,
       password,
+      headcount: headCount,
+      court: area,
+      startDate: startValue,
+      endDate: endValue,
     };
 
     console.log(postData);
 
     await axios
-      .post("/reservation/court", { postData })
+      .post('http://localhost:7000/reservation', postData)
       .then((res) => {
         Swal.fire({
-          icon: "success",
-          title: "예약 성공",
+          icon: 'success',
+          title: '예약 성공',
           showConfirmButton: false,
           timer: 1000,
         });
         handleNext();
       })
       .catch((err) => {
+        console.log(err);
         Swal.fire({
-          icon: "error",
-          iconColor: "#d32f2f",
-          title: "예약 실패",
-          text: "다시 시도해주세요",
-          confirmButtonColor: "#005cb8",
+          icon: 'error',
+          iconColor: '#d32f2f',
+          title: '예약 실패',
+          text: '다시 시도해주세요',
+          confirmButtonColor: '#005cb8',
         });
       });
   }
@@ -98,21 +99,21 @@ export default function Checkout() {
   const [startValue, setStartValue] = useState(new Date());
   const [endValue, setEndValue] = useState(new Date());
   const [headCount, setCount] = useState(6);
-  const [area, setArea] = useState("RIGHT");
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [password, setPassword] = useState("");
+  const [area, setArea] = useState('RIGHT');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
 
   // 재확인용 비밀번호 State
-  const [rePassword, setRePassword] = useState("");
+  const [rePassword, setRePassword] = useState('');
 
   // 유효성 검사
-  const [nameError, setNameError] = useState("");
-  const [phoneError, setPhoneError] = useState("");
-  const [passwordState, setPasswordState] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+  const [nameError, setNameError] = useState('');
+  const [phoneError, setPhoneError] = useState('');
+  const [passwordState, setPasswordState] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
-  const buttonValue = activeStep === steps.length - 1 ? "예약하기" : "다음";
+  const buttonValue = activeStep === steps.length - 1 ? '예약하기' : '다음';
 
   // 이용약관 동의 state
   const [checked, setChecked] = useState(false);
@@ -165,7 +166,7 @@ export default function Checkout() {
           />
         );
       default:
-        throw new Error("Unknown step");
+        throw new Error('Unknown step');
     }
   }
 
@@ -186,10 +187,10 @@ export default function Checkout() {
     if (activeStep === 0) {
       if (!checked) {
         Swal.fire({
-          icon: "error",
-          iconColor: "#d32f2f",
-          title: "개인정보 약관에 동의해주세요.",
-          confirmButtonColor: "#005cb8",
+          icon: 'error',
+          iconColor: '#d32f2f',
+          title: '개인정보 약관에 동의해주세요.',
+          confirmButtonColor: '#005cb8',
         });
       } else handleNext();
     }
@@ -198,25 +199,25 @@ export default function Checkout() {
       // 이름 유효성 체크
       const nameRegex = /^[가-힣a-zA-Z]+$/;
       if (!nameRegex.test(name) || name.length < 1)
-        setNameError("올바른 이름을 입력해주세요.");
-      else setNameError("");
+        setNameError('올바른 이름을 입력해주세요.');
+      else setNameError('');
 
       // 전화번호 유효성 체크
       const phoneRegex = /^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$/;
       if (!phoneRegex.test(phone))
-        setPhoneError("올바른 전화번호를 입력해주세요.");
-      else setPhoneError("");
+        setPhoneError('올바른 전화번호를 입력해주세요.');
+      else setPhoneError('');
 
       // 비밀번호 유효성 체크
       const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
       if (!passwordRegex.test(password))
-        setPasswordState("영문자+숫자 조합으로 8자리 이상 입력해주세요.");
-      else setPasswordState("");
+        setPasswordState('영문자+숫자 조합으로 8자리 이상 입력해주세요.');
+      else setPasswordState('');
 
       // 비밀번호 같은지 체크
       if (password !== rePassword)
-        setPasswordError("비밀번호가 일치하지 않습니다.");
-      else setPasswordError("");
+        setPasswordError('비밀번호가 일치하지 않습니다.');
+      else setPasswordError('');
 
       // 유효성 체크 후 통과시 다음으로 넘어가기
       if (
@@ -233,7 +234,7 @@ export default function Checkout() {
 
     // 마지막에서 예약하기 버튼일 경우 onhandlePost
     if (activeStep === 2) {
-      if (event.target.id === "예약하기") {
+      if (event.target.id === '예약하기') {
         onhandlePost(userData);
       }
     }
@@ -244,9 +245,9 @@ export default function Checkout() {
     <ThemeProvider theme={theme}>
       <Header />
       <FadeIn>
-        <Container component="main" maxWidth="md">
-          <Paper variant="outlined" sx={{ p: { xs: 2, md: 5 } }}>
-            <Typography variant="h4" align="center">
+        <Container component='main' maxWidth='md'>
+          <Paper variant='outlined' sx={{ p: { xs: 2, md: 5 } }}>
+            <Typography variant='h4' align='center'>
               농구장 예약
             </Typography>
             <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 3 }}>
@@ -259,20 +260,19 @@ export default function Checkout() {
             <React.Fragment>
               {activeStep === steps.length ? (
                 <React.Fragment>
-                  <Typography variant="h5" gutterBottom>
+                  <Typography variant='h5' gutterBottom>
                     예약해주셔서 감사합니다.
                   </Typography>
-                  <Typography variant="subtitle1">
+                  <Typography variant='subtitle1'>
                     예약은 "내 예약 확인하기"에서 다시 확인하실 수 있습니다.
                   </Typography>
                   <Button
-                    type="submit"
-                    variant="contained"
+                    type='submit'
+                    variant='contained'
                     sx={{ mt: 3 }}
                     onClick={() => {
-                      navigate("/check");
-                    }}
-                  >
+                      navigate('/check');
+                    }}>
                     내 예약 확인하기
                   </Button>
                 </React.Fragment>
@@ -280,11 +280,10 @@ export default function Checkout() {
                 <React.Fragment>
                   {getStepContent(activeStep)}
                   <Box
-                    sx={{ display: "flex", justifyContent: "flex-end" }}
+                    sx={{ display: 'flex', justifyContent: 'flex-end' }}
                     onSubmit={handleSubmit}
-                    component="form"
-                    noValidate
-                  >
+                    component='form'
+                    noValidate>
                     {activeStep !== 0 && (
                       <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
                         이전
@@ -293,11 +292,10 @@ export default function Checkout() {
 
                     <Button
                       id={buttonValue}
-                      type="submit"
-                      variant="contained"
+                      type='submit'
+                      variant='contained'
                       sx={{ mt: 3, ml: 1 }}
-                      onClick={(event) => handleSubmit(event)}
-                    >
+                      onClick={(event) => handleSubmit(event)}>
                       {buttonValue}
                     </Button>
                   </Box>
